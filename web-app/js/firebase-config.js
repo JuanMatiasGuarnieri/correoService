5d60a400e8d17546a2237ee0c052407379188d91
Juan Matias Guarnieri <70146381+JuanMatiasGuarnieri@users.noreply.github.com>
2026-05-17 16:49:41 -0300

    Update Firebase configuration with placeholder values

--- web-app/js/firebase-config.js
+++ web-app/js/firebase-config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDsW0BOCa-O90zy2RfuEfLACSBXOHFjwGE",
-    authDomain: "correo-fleet.firebaseapp.com",
-    databaseURL: "https://correo-fleet-default-rtdb.firebaseio.com",
-    projectId: "correo-fleet",
-    storageBucket: "correo-fleet.firebasestorage.app",
+    apiKey: "API KEY DE FIREBASE",
+    authDomain: "DOMINIO DE LA BD DE FIREBASE",
+    databaseURL: "URL DE LA BASE DE DATOS DE FIREBASE",
+    projectId: "ID DE PROYECTO",
+    storageBucket: "STORAGE DE FIREBASE",
     messagingSenderId: "851994949651",
     appId: "1:851994949651:web:836e718f630aadf7aaa7e6",
     measurementId: "G-WN36724L4C"
